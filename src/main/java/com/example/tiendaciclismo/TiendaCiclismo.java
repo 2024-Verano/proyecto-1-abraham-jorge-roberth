eaff1600c39d611e5c009161d8b35a5b7f945154
Roberth Rojas Quesada <40683721+rorojasq@users.noreply.github.com>
2024-12-21 21:02:26 -0600

    Crear proyecto

---
+++ src/main/java/com/example/tiendaciclismo/TiendaCiclismo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.example.tiendaciclismo;
+
+/**
+ *
+ * @author universidad
+ */
+public class TiendaCiclismo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
