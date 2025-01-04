723b663a12b40c021624e55d2c21bac4107f40be
Roberth Rojas Quesada <40683721+rorojasq@users.noreply.github.com>
2025-01-03 23:20:17 -0600

    Añadir constructor y funciones get/set

--- src/main/java/com/example/tiendaciclismo/ServicioMantenimiento.java
+++ src/main/java/com/example/tiendaciclismo/ServicioMantenimiento.java
@@ -49,4 +49,66 @@ class ServicioMantenimiento {
      * Indica si el servicio está Abierto o Cerrado.  Una vez cerrado, se pasará a facturación.
      */
     private EstadoMantenimiento estado;
+
+
+    public ServicioMantenimiento(long codigoServicio, long codigoCliente) {
+        this.codigoServicio = codigoServicio;
+        this.codigoCliente = codigoCliente;
+    }
+
+    public long getCodigoServicio() {
+        return codigoServicio;
+    }
+
+    public long getCodigoCliente() {
+        return codigoCliente;
+    }
+
+    public String getMarcaBicicleta() {
+        return marcaBicicleta;
+    }
+
+    public void setMarcaBicicleta(String marcaBicicleta) {
+        this.marcaBicicleta = marcaBicicleta;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public LocalDateTime getFechaRecibido() {
+        return fechaRecibido;
+    }
+
+    public void setFechaRecibido(LocalDateTime fechaRecibido) {
+        this.fechaRecibido = fechaRecibido;
+    }
+
+    public LocalDateTime getFechaEntrega() {
+        return fechaEntrega;
+    }
+
+    public void setFechaEntrega(LocalDateTime fechaEntrega) {
+        this.fechaEntrega = fechaEntrega;
+    }
+
+    public String getObservaciones() {
+        return observaciones;
+    }
+
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
+
+    public EstadoMantenimiento getEstado() {
+        return estado;
+    }
+
+    public void setEstado(EstadoMantenimiento estado) {
+        this.estado = estado;
+    }
 }
