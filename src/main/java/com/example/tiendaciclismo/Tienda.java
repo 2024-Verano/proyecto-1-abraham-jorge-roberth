49d8583418eedc5873e506df310a4ce5d0193587
Roberth Rojas Quesada <40683721+rorojasq@users.noreply.github.com>
2025-01-05 15:24:35 -0600

    Crear código para inicio de sesión

--- src/main/java/com/example/tiendaciclismo/Tienda.java
+++ src/main/java/com/example/tiendaciclismo/Tienda.java
@@ -9,4 +9,25 @@ package com.example.tiendaciclismo;
  * @author Roberth Rojas
  */
 public class Tienda {
+    private Boolean autenticado = false;
+
+    private ControlAcceso acceso;
+
+    public Tienda() throws ControlAccesoException {
+        acceso = new ControlAcceso();
+    }
+
+    public Boolean getAutenticado() {
+        return autenticado;
+    }
+
+    public Boolean iniciarSesion(String nombreUsuario, String contrasena) {
+        autenticado = acceso.iniciarSesion(nombreUsuario, contrasena);
+
+        return autenticado;
+    }
+
+    public void cerrarSesion() {
+        autenticado = false;
+    }
 }
