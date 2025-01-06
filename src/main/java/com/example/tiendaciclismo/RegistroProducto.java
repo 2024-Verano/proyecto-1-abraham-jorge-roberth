3fae4b2d1a686c76643d31e584a6ab7b44a7b5ad
Roberth Rojas Quesada <40683721+rorojasq@users.noreply.github.com>
2025-01-06 05:05:21 -0600

    Borrar imports sin usar

--- src/main/java/com/example/tiendaciclismo/RegistroProducto.java
+++ src/main/java/com/example/tiendaciclismo/RegistroProducto.java
@@ -2,31 +2,11 @@ package com.example.tiendaciclismo;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.io.File;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-
 import com.example.tiendaciclismo.almacenamiento.XML;
 
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import org.w3c.dom.DOMException;
 
 class RegistroProducto {
     /**
