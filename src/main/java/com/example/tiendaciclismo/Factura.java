528bec2bd9c366dd76670dc27d26074a89738126
Roberth Rojas Quesada <40683721+rorojasq@users.noreply.github.com>
2025-01-06 05:55:02 -0600

    Simplificar Factura

--- src/main/java/com/example/tiendaciclismo/Factura.java
+++ src/main/java/com/example/tiendaciclismo/Factura.java
@@ -1,13 +1,44 @@
 package com.example.tiendaciclismo;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 
 class Factura {
     /**
-     * El encabezado de la factura
+     * Identificador único asignado secuencialmente.
      */
-    private EncabezadoFactura encabezado;
+    private long numFactura;
+
+    /**
+     * Valor numérico que pertenece a Clientes
+     */
+    private Cliente codigoCliente;
+
+    /**
+     * Fecha en que se recibió.
+     */
+    private LocalDateTime fechaRecibido;
+
+    /**
+     * El estado valido o anulado. Este valor no puede ser manipulado por el usuario.
+     */
+    private boolean estado;
+
+    /**
+     * Valor antes de los impuestos
+     */
+    private int subtotal;
+
+    /**
+     * El 13% del de la suma de los artculos que se facture
+     */
+    private int impuesto;
+
+    /**
+     * El subtotal+impuestos.
+     */
+    private int total;
 
     /**
      * El detalle de la factura
