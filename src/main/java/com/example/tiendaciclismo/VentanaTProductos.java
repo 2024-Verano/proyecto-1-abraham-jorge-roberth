33520261dfdb1ebeba595b24499790e3d935dd51
Roberth Rojas Quesada <40683721+rorojasq@users.noreply.github.com>
2025-01-06 04:05:49 -0600

    Mostrar ventana de tipos de producto

--- src/main/java/com/example/tiendaciclismo/VentanaTProductos.java
+++ src/main/java/com/example/tiendaciclismo/VentanaTProductos.java
@@ -865,7 +865,7 @@ public class VentanaTProductos extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
+    public static void create() {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
