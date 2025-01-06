f0c6bc4470128f2b8a6db1750225f555eeac6b13
Roberth Rojas Quesada <40683721+rorojasq@users.noreply.github.com>
2025-01-06 00:25:43 -0600

    Agregar botón para cerrar la aplicación

--- src/main/java/com/example/tiendaciclismo/VentanaPrincipal.java
+++ src/main/java/com/example/tiendaciclismo/VentanaPrincipal.java
@@ -28,6 +28,7 @@ public class VentanaPrincipal extends javax.swing.JFrame {
 
                 jButton1 = new javax.swing.JButton();
                 jButton3 = new javax.swing.JButton();
+                jButton5 = new javax.swing.JButton();
 
                 setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -45,6 +46,13 @@ public class VentanaPrincipal extends javax.swing.JFrame {
                         }
                 });
 
+                jButton5.setText("Salir");
+                jButton5.addActionListener(new java.awt.event.ActionListener() {
+                        public void actionPerformed(java.awt.event.ActionEvent evt) {
+                                jButton5ActionPerformed(evt);
+                        }
+                });
+
                 javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
                 getContentPane().setLayout(layout);
                 layout.setHorizontalGroup(
@@ -52,6 +60,7 @@ public class VentanaPrincipal extends javax.swing.JFrame {
                         .addGroup(layout.createSequentialGroup()
                                 .addGap(99, 99, 99)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addComponent(jButton5, javax.swing.GroupLayout.PREFERRED_SIZE, 246, javax.swing.GroupLayout.PREFERRED_SIZE)
                                         .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 246, javax.swing.GroupLayout.PREFERRED_SIZE)
                                         .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 246, javax.swing.GroupLayout.PREFERRED_SIZE))
                                 .addContainerGap(93, Short.MAX_VALUE))
@@ -63,7 +72,9 @@ public class VentanaPrincipal extends javax.swing.JFrame {
                                 .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 53, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addGap(22, 22, 22)
                                 .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 53, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addContainerGap(356, Short.MAX_VALUE))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 252, Short.MAX_VALUE)
+                                .addComponent(jButton5, javax.swing.GroupLayout.PREFERRED_SIZE, 53, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(51, 51, 51))
                 );
 
                 pack();
@@ -76,6 +87,10 @@ public class VentanaPrincipal extends javax.swing.JFrame {
         private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
                 // TODO add your handling code here:
         }//GEN-LAST:event_jButton3ActionPerformed
+
+        private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
+		System.exit(0);
+        }//GEN-LAST:event_jButton5ActionPerformed
 
 	/**
 	 * @param args the command line arguments
@@ -115,5 +130,6 @@ public class VentanaPrincipal extends javax.swing.JFrame {
         // Variables declaration - do not modify//GEN-BEGIN:variables
         private javax.swing.JButton jButton1;
         private javax.swing.JButton jButton3;
+        private javax.swing.JButton jButton5;
         // End of variables declaration//GEN-END:variables
 }
