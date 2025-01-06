9c3f994b25978c51ea0e559807ca730420a9478c
Roberth Rojas Quesada <40683721+rorojasq@users.noreply.github.com>
2025-01-06 04:55:45 -0600

    Mostrar ventana de productos

--- src/main/java/com/example/tiendaciclismo/ventanaProdu.java
+++ src/main/java/com/example/tiendaciclismo/ventanaProdu.java
@@ -795,7 +795,7 @@ String codigoStr = sameTf.getText().trim();
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
+    public static void create() {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
