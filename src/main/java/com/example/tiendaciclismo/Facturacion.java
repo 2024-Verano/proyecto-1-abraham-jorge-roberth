9ab674294864ee927b924e5fbb33b8029e0860e2
Roberth Rojas Quesada <40683721+rorojasq@users.noreply.github.com>
2025-01-06 06:41:01 -0600

    Implementar métodos de facturación

--- src/main/java/com/example/tiendaciclismo/Facturacion.java
+++ src/main/java/com/example/tiendaciclismo/Facturacion.java
@@ -1,9 +1,19 @@
 package com.example.tiendaciclismo;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 
 class Facturacion {
+
+    /**
+     * Guarda el último código de servicio usado.
+     * Cuando se almacena un registro, se incrementa en uno.
+     * Al cargarse la lista de registros, el código del último registro se almacena aquí.
+     */
+    private static long ultimoNumeroFactura = 0;
+
     /**
      * Lista de facturas.
      */
@@ -12,35 +22,68 @@ class Facturacion {
     /**
      * Crea una factura
      */
-    public Factura agregarFactura() throws Exception {
-        throw new Exception("Sin implementar");
+    public Factura agregarFactura(long codigoCliente, LocalDateTime fechaRecibido, int subtotal, int impuesto, int total) {
+        Factura factura = new Factura(ultimoNumeroFactura + 1, codigoCliente, fechaRecibido, subtotal, impuesto, total);
+        facturas.add(factura);
+
+        return factura;
     }
 
     /**
      * Busca una factura por numero.
      */
-    public Factura buscarPorNumero() throws Exception {
-        throw new Exception("Sin implementar");
+    public Factura buscarPorNumero(long numeroFactura) {
+        Factura resultado = null;
+        for (Factura factura : facturas) {
+            if (factura.getNumero() == numeroFactura) {
+                resultado = factura;
+                break;
+            }
+        }
+
+        return resultado;
     }
 
     /**
      * Busca facturas por fecha.
      */
-    public ArrayList<Factura> buscarPorFecha() throws Exception {
-        throw new Exception("Sin implementar");
+    public ArrayList<Factura> buscarPorFecha(LocalDate fecha) {
+        ArrayList<Factura> resultados = new ArrayList<>();
+
+        for (Factura factura : facturas) {
+            if (factura.getFechaRecibido().toLocalDate().equals(fecha)) {
+                resultados.add(factura);
+            }
+        }
+
+        return resultados;
+    }
+
+    public void agregarDetalle(long numeroFactura, long codigoArticulo, int cantidad, int precioUnidad) {
+        Factura factura = buscarPorNumero(numeroFactura);
+        factura.agregarDetalle(codigoArticulo, cantidad, precioUnidad);
     }
 
     /**
-     * Busca facturas por el nombre de cliente.
+     * Busca facturas por el código de cliente.
      */
-    public ArrayList<Factura> buscarPorCliente() throws Exception {
-        throw new Exception("Sin implementar");
+    public ArrayList<Factura> buscarPorCliente(long codigoCliente) {
+        ArrayList<Factura> resultados = new ArrayList<>();
+
+        for (Factura factura : facturas) {
+            if (factura.getNumero() == codigoCliente) {
+                resultados.add(factura);
+            }
+        }
+
+        return resultados;
     }
 
     /**
      * Anula una factura.
      */
-    public void anularFactura() throws Exception {
-        throw new Exception("Sin implementar");
+    public void anularFactura(long numeroFactura) {
+        Factura factura = buscarPorNumero(numeroFactura);
+        factura.setAnulado(true);
     }
 }
